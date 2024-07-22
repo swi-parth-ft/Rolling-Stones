6ec50a38886bf25182077cc0bed4720e0a52756f
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-22 18:08:44 -0400

    Initial Commit

---
+++ Rolling Stones/Rolling_StonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rolling_StonesApp.swift
+//  Rolling Stones
+//
+//  Created by Parth Antala on 2024-07-22.
+//
+
+import SwiftUI
+
+@main
+struct Rolling_StonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
